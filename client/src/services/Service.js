0d02584cf728bdc4e1df98a2024c2d9efa0dd95c
Joel <jcdghtn@gmail.com>
2020-10-21 10:52:23 -0300

    changed function to async in service.js

--- client/src/services/Service.js
+++ client/src/services/Service.js
@@ -16,28 +16,25 @@ const BASE_URL = 'http://localhost:3333';
 //     return res.data;
 //   },
 
-export {getTopTracks};
-export {getTopArtists};
-export {getRecentlyPlayed};
-export {getRecommendations};
+export default {
+  async getTopTracks() {
+    const url = `${BASE_URL}/tracks`;
+    return axios.get(url).then(response => response.data);
+  },
 
-function getTopTracks() {
-  const url = `${BASE_URL}/tracks`;
-  return axios.get(url).then(response => response.data);
-}
+  async getTopArtists() {
+    const url = `${BASE_URL}/artists`;
+    return axios.get(url).then(response => response.data);
+  },
 
-function getTopArtists() {
-  const url = `${BASE_URL}/artists`;
-  return axios.get(url).then(response => response.data);
-}
+  async getRecentlyPlayed() {
+    const url = `${BASE_URL}/recently-played`;
+    return axios.get(url).then(response => response.data);
+  },
 
-function getRecentlyPlayed() {
-  const url = `${BASE_URL}/recently-played`;
-  return axios.get(url).then(response => response.data);
-}
-
-function getRecommendations() {
-  const url = `${BASE_URL}/recommendations`;
-  return axios.get(url).then(response => response.data);
-}
+  async getRecommendations() {
+    const url = `${BASE_URL}/recommendations`;
+    return axios.get(url).then(response => response.data);
+  },
+};
 // }
